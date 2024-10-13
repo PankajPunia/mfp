67214bf2def3cf0c2f47452887609e38118a42cf
Pankaj-Punia <pankajpunia0029@gmail.com>
2024-10-13 19:46:25 -0400

    add styles classname

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
-import Header from './components/Header';
-
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
   return (
-    <BrowserRouter>
-      <Header/>
-      <MarketingApp />
-    </BrowserRouter>
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <Header />
+          <MarketingApp />
+        </BrowserRouter>
+      </StylesProvider>
+    </div>
   );
 };
